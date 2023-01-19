f2bd4225cfff1539a11c42dd831dc402fcad9cca
Kianoosh <kia.comp.eng@outlook.com>
2023-01-20 01:40:48 +0330

    login and signup is completed now

---
+++ project/linkedInProject/main.py
@@ -0,0 +1,41 @@
+from User import User
+
+user = User()
+
+def showMenu():
+    print('1.LogIn\n2.SignUp\n3.Show All users\n4.Search')
+    inp = input('>> ')
+    return inp
+def login(username , password):
+    if User.findInFile(username , password , User._local_users_file_path) != None:
+        #go to login
+        print()
+    else:
+        print('User not found!')
+        showMenu()
+
+def signup(username, password, name, dateOfBirth, universityLocation, field, workplace, specialties):
+    u = User()
+    u.setData1(username, password, name, dateOfBirth, universityLocation, field, workplace, specialties)
+    u.saveFile(User._local_users_file_path)
+
+
+
+print('Welcome to Unlinked Out')
+
+inp = showMenu()
+
+if inp == '1':
+    username = input('Enter Username >> ')
+    password = input('Enter Password >> ')
+    login(username , password)
+elif inp == '2':
+    name = input('Name >> ')
+    dateOfBirth = input('Date of birth >> ')
+    universityLocation = input('University location >> ')
+    field = input('Field >> ')
+    workplace = input('Work place >> ')
+    specialties = list(input('Specialties >> ').split())
+    username = input('username >> ')
+    password = input('password >> ')
+    signup(username, password, name, dateOfBirth, universityLocation, field, workplace, specialties)
