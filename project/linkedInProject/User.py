56988122da3750199c3f48ac0b4267056ef3269b
Kianoosh <kia.comp.eng@outlook.com>
2023-01-20 00:27:13 +0330

    User model is nearly completed !

--- project/linkedInProject/User.py
+++ project/linkedInProject/User.py
@@ -1,6 +1,24 @@
+import json
+import os.path
+from collections import namedtuple
+import demjson3
+
+from types import SimpleNamespace as Namespace
+
+
 class User :
+    _main_users_file_path = '../Files/users2.json'
+    _local_users_file_path = '../Files/users.json'
     id = str()
-    def __int__(self , username, password, name, dateOfBirth,  universityLocation,  field,  workplace, specialties):
+    username = str()
+    password = str()
+    name = str()
+    dateOfBirth = str()
+    universityLocation = str()
+    field = str()
+    workplace = str()
+    specialties = str()
+    def __int__(self , username, password, name, dateOfBirth, universityLocation, field, workplace, specialties):
         self.username = username
         self.password = password
         self.name = name
@@ -9,4 +27,69 @@ class User :
         self.field = field
         self.workplace = workplace
         self.specialties = specialties
+    def setData1(self , username, password, name, dateOfBirth, universityLocation, field, workplace, specialties):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.dateOfBirth = dateOfBirth
+        self.universityLocation = universityLocation
+        self.field = field
+        self.workplace = workplace
+        self.specialties = specialties
+    def setData(self , name, dateOfBirth, universityLocation, field, workplace, specialties):
+        self.name = name
+        self.dateOfBirth = dateOfBirth
+        self.universityLocation = universityLocation
+        self.field = field
+        self.workplace = workplace
+        self.specialties = specialties
 
+    def getUsers(path): # static function
+        f = open(path)
+        data = json.load(f)
+        users = list()
+
+        for item in data:
+            u = User()
+            u.setData(item['name'] , item['dateOfBirth'] , item['universityLocation'] , item['field'] , item['workplace'] , item['specialties'])
+            users.append(u)
+        return users
+
+    def saveFile(self , path):
+
+        if os.stat(path).st_size == 0: #to avoid reading empty file
+            tmp = open(path , 'w')
+            tmp.write('[]')
+            tmp.close()
+
+        f = open(path)
+        data = json.load(f)
+        f.close()
+        f = open(path , 'w')
+        data.append(self.__dict__)
+        #print(json.dumps(self.__dict__))
+        f.write(json.dumps(data))
+        print(self.__dict__)
+        f.close()
+
+    def findInFile(username , password , path):
+        f = open(path)
+        data = json.load(f)
+        f.close()
+        for item in data:
+            if item['username'] == username and item['password'] == password:
+                u = User()
+                u.setData1(item['username'], item['password'],item['name'], item['dateOfBirth'], item['universityLocation'], item['field'],item['workplace'], item['specialties'])
+                return u
+        return None
+
+    def isInFile(name , path):
+        f = open(path)
+        data = json.load(f)
+        f.close()
+        for item in data:
+            if item['name'] == name:
+                return True
+        return False
+
+print(User.isInFile('tannaz Fitzgerald', User._main_users_file_path))
